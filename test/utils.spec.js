7dfde5f344c19942937ac446d58faa7e49cc6ca1
fabnguess <fabnguess@gmail.com>
2023-12-03 11:35:29 +0000

    chore(test): migrate from tape to test_runner

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,17 +1,14 @@
-// Import Third-party Dependencies
-import test from "tape";
+// Import Node.js Dependencies
+import { test } from "node:test";
+import assert from "node:assert";
 
 // Import Internal Dependency
 import { getRepositoryPath } from "../src/utils.js";
 
-test("getRepositoryPath must return id", (tape) => {
-  tape.is(getRepositoryPath("10"), "10");
-
-  tape.end();
+test("getRepositoryPath must return id", () => {
+  assert.equal(getRepositoryPath("10"), "10");
 });
 
-test("getRepositoryPath must return gitlab path", (tape) => {
-  tape.is(getRepositoryPath("nodesecure.boo"), "nodesecure%2Fboo");
-
-  tape.end();
+test("getRepositoryPath must return gitlab path", () => {
+  assert.equal(getRepositoryPath("nodesecure.boo"), "nodesecure%2Fboo");
 });
