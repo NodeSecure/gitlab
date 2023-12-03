f51e5aba9864ffa799c271d6568c8308d06ca936
Kouadio Fabrice Nguessan <72697416+fabnguess@users.noreply.github.com>
2023-12-03 12:10:47 +0000

    chore(test): migrate from tape to test_runner (#186)

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 // Import Node.js Dependencies
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs/promises";
+import { test } from "node:test";
+import assert from "node:assert";
 
 // Import Third-party Dependencies
-import test from "tape";
 import is from "@slimio/is";
 
 // Import Internal Dependency
@@ -19,65 +17,51 @@ const kDownloadDir = path.join(__dirname, "downloads");
 
 await fs.mkdir(kDownloadDir);
 
-test("gitlab.download should be an asyncFunction", (tape) => {
-  tape.true(is.func(gitlab.download));
-  tape.true(is.asyncFunction(gitlab.download));
-
-  tape.end();
+test("gitlab.download should be an asyncFunction", () => {
+  assert.equal(is.func(gitlab.download), true);
+  assert.equal(is.asyncFunction(gitlab.download), true);
 });
 
-test("gitlab.downloadAndExtract should be an asyncFunction", (tape) => {
-  tape.true(is.func(gitlab.downloadAndExtract));
-  tape.true(is.asyncFunction(gitlab.downloadAndExtract));
-
-  tape.end();
+test("gitlab.downloadAndExtract should be an asyncFunction", () => {
+  assert.equal(is.func(gitlab.downloadAndExtract), true);
+  assert.equal(is.asyncFunction(gitlab.downloadAndExtract), true);
 });
 
-test("download must throw: repository must be a string!", async(tape) => {
-  tape.plan(2);
-
-  try {
-    await gitlab.download(10);
-  }
-  catch (error) {
-    tape.strictEqual(error.name, "TypeError");
-    tape.strictEqual(error.message, "repository must be a string!");
-  }
-
-  tape.end();
+test("download must throw: repository must be a string!", async() => {
+  await assert.rejects(
+    async() => await gitlab.download(10),
+    {
+      name: "TypeError",
+      message: "repository must be a string!"
+    }
+  );
 });
 
-test("extract tar.gz at in the current working dir", async(tape) => {
+test("extract tar.gz at in the current working dir", async() => {
   const { location } = await gitlab.download("polychromatic.plombier-chauffagiste");
 
   await fs.access(location);
-  tape.strictEqual(path.extname(location), ".gz");
-
-  tape.end();
+  assert.strictEqual(path.extname(location), ".gz");
 });
 
-test("download and extract a public gitlab repository", async(tape) => {
+test("download and extract a public gitlab repository", async() => {
   const { location } = await gitlab.downloadAndExtract("polychromatic.plombier-chauffagiste", {
     dest: kDownloadDir
   });
 
-  await fs.access(location);
-  tape.pass();
-
-  tape.end();
+  await assert.doesNotReject(
+    async() => await fs.access(location)
+  );
 });
 
-test("teardown", async(tape) => {
+test("teardown", async() => {
   await new Promise((resolve) => setImmediate(resolve));
-  await fs.rm(kDownloadDir, { recursive: true, force: true });
 
-  try {
-    await fs.unlink(path.join(process.cwd(), "plombier-chauffagiste-master.tar.gz"));
-  }
-  catch (err) {
-    // do nothing
-  }
-  finally {
-    tape.end();
-  }
+  await assert.doesNotReject(
+    async() => await fs.rm(kDownloadDir, { recursive: true, force: true })
+  );
+
+  await assert.doesNotReject(
+    async() => await fs.unlink(path.join(process.cwd(), "plombier-chauffagiste-master.tar.gz"))
+  );
 });
