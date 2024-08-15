a2355327d4b050745d991a88c476d9f051aa39c7
fraxken <gentilhomme.thomas@gmail.com>
2024-08-15 15:50:07 +0200

    test: split for readability and use mkdtemp & os.tmpdir

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,14 +1,16 @@
 // Import Node.js Dependencies
-import { test } from "node:test";
+import { describe, test } from "node:test";
 import assert from "node:assert";
 
 // Import Internal Dependency
 import { getRepositoryPath } from "../src/utils.js";
 
-test("getRepositoryPath must return id", () => {
-  assert.equal(getRepositoryPath("10"), "10");
-});
+describe("getRepositoryPath", () => {
+  test("must return id", () => {
+    assert.strictEqual(getRepositoryPath("10"), "10");
+  });
 
-test("getRepositoryPath must return gitlab path", () => {
-  assert.equal(getRepositoryPath("nodesecure.boo"), "nodesecure%2Fboo");
+  test("must return GitLab path (encoded for URL)", () => {
+    assert.strictEqual(getRepositoryPath("nodesecure.boo"), "nodesecure%2Fboo");
+  });
 });
