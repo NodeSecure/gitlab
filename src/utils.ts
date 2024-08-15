0a437e19bf46bdc1acd1c4a368cec27d8856c5ab
fraxken <gentilhomme.thomas@gmail.com>
2024-08-15 13:48:37 +0200

    refactor!: migrate to TypeScript and enhance API

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,9 @@
  * getRepositoryPath("51886657"); // "51886657"
  * getRepositoryPath("myorg.foo.bar") // "myorg%2Ffoo%2Fbar"
  */
-export function getRepositoryPath(repository) {
+export function getRepositoryPath(
+  repository: string
+): string {
   const repoId = Number.parseInt(repository, 10);
 
   return Number.isNaN(repoId) ? repository.split(".").join("%2F") : String(repoId);
